f217e8106a578c84ce1e7a95b4d9c8ab096670a5
Simon Berger <sbe@facelift-bbt.com>
2023-01-28 16:04:30 +0100

    init

---
+++ src/Core/Tests/Unit/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace GroupsProblem\Core\Tests\Unit;
+
+class Test extends \Codeception\Test\Unit
+{
+    public function testDummy(): void
+    {
+        $this->assertNull(null);
+    }
+}
